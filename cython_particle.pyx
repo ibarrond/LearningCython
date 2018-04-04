10a11f9a97851472a511462a0c6b5d08323f924e
IBARRONDO <alberto.ibarrondo@sap.com>
2018-04-04 17:05:16 +0200

    Chapter 5

---
+++ cython_particle.pyx
@@ -0,0 +1,8 @@
+cdef class Particle:
+	cdef double mass, position, velocity
+	def __init__(self, m, p, v):
+		self.mass = m
+		self.position = p
+		self.velocity = v
+	def get_momentum(self):
+		return self.mass * self.velocity
